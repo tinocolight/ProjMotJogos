f11f216757c8f31178f9ca394e6a6fe38a282f18
Rui Luz <a17984@alunos.ipca.pt>
2020-01-11 15:46:14 +0000

    no message

---
+++ Game1/Game1/MessageHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Game1
+{
+    public abstract class MessageHandler
+    {
+         public abstract void HandleMessage(Message message);
+    }
+}
